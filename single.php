65c3ce3c165488763339a0a01543f71be68b60a6
nunumare <58900289+nunumare@users.noreply.github.com>
2020-01-31 23:27:13 -0800

    Add files via upload

--- single.php
+++ single.php
@@ -1,35 +1,37 @@
 <?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package school
+ */
+
 get_header();
-if(have_posts()) :
-    while(have_posts()): the_post();?>
-    <article class = "post">
-        <h2><a href = "<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <p class = "post-info"><?php the_time('F jS, Y g:i a'); ?> | by <a href = "<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author();?></a> | Posted in
-            <?php
-            $categories = get_the_category();
-            $seperator = ", ";
-            $output = '';
-
-            if($categories){
-                foreach($categories as $category){
-                    $output .= '<a href = "' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $seperator;
-                }
-                echo trim($output, $seperator);
-            }
-
-         ?>   
-        </p>
-        <?php the_post_thumbnail('banner-image'); ?>
-        <?php the_content(); ?>
-    </article>
-
-          
-    <?php endwhile;
-else:
-    echo '<p>No content found</p>'; 
-
-    endif;
-get_footer();
-
-
 ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			the_post_navigation();
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
